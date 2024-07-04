429fe68d8ae9a25c2d3ecee765be5b76321b50df
mflkee <makeevgleb86rus@gmail.com>
2024-07-05 00:24:35 +0500

    add wifi togle

--- polybar/scripts/wifi_switch.sh
+++ polybar/scripts/wifi_switch.sh
@@ -2,8 +2,12 @@
 
 state=$(nmcli radio wifi)
 
+echo "Current Wi-Fi state: $state"
+
 if [[ "$state" == "enabled" ]]; then
+    echo "Turning Wi-Fi off"
     nmcli radio wifi off
 else
+    echo "Turning Wi-Fi on"
     nmcli radio wifi on
 fi
