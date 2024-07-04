8b4b97c0f30f66c43ab1618aec40192ed7d23e1d
mflkee <makeevgleb86rus@gmail.com>
2024-07-04 18:36:40 +0500

    add files

--- sxhkd/scripts/record.sh
+++ sxhkd/scripts/record.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Функция для создания директории, если она не существует
+create_dir_if_not_exists() {
+  local dir="$1"
+  if [ ! -d "$dir" ]; then
+    mkdir -p "$dir"
+    echo "Directory created: $dir"
+  fi
+}
+
+# Путь к директории для сохранения записей
+record_dir="/home/mflkee/videos/record"
+
+# Создать директорию, если она не существует
+create_dir_if_not_exists "$record_dir"
+
 # Проверить, запущена ли запись
 if pgrep -f "ffmpeg -f x11grab -i :0.0 -r 30 -vcodec libx264 -acodec libmp3lame -y" > /dev/null; then
   # Запись запущена, остановить ее
@@ -7,7 +22,8 @@ if pgrep -f "ffmpeg -f x11grab -i :0.0 -r 30 -vcodec libx264 -acodec libmp3lame 
   echo "Запись остановлена!"
 else
   # Запись не запущена, запустить ее
-  ffmpeg -f x11grab -i :0.0 -r 30 -vcodec libx264 -acodec libmp3lame -y "/home/mflkee/video/record/record_$(date +%Y-%m-%d_%H-%M-%S).mp4" &
+  filename="record_$(date +%Y-%m-%d_%H-%M-%S).mp4"
+  full_path="$record_dir/$filename"
+  ffmpeg -f x11grab -i :0.0 -r 30 -vcodec libx264 -acodec libmp3lame -y "$full_path" &
   echo "Запись началась!"
 fi
-
